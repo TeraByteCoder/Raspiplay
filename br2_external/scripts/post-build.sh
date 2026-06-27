648fc8a638b4a09eeda3669fff48cda4d7bffcce
lukas <hamburgerlukasofficial@gmail.com>
2026-06-27 22:52:38 +0200

    Add Wi-Fi fallback hotspot and captive portal

--- br2_external/scripts/post-build.sh
+++ br2_external/scripts/post-build.sh
@@ -11,6 +11,7 @@ mkdir -p "$target_dir/run" "$target_dir/tmp" "$target_dir/var/lib/misc" \
   "$target_dir/var/lib/bluetooth" "$target_dir/var/log" "$target_dir/var/run"
 
 rm -f "$target_dir/etc/init.d/S80dnsmasq"
+rm -f "$target_dir/etc/init.d/S90httpd"
 
 find "$target_dir/etc/init.d" -type f -name 'S*' -exec chmod 0755 {} +
 find "$target_dir/usr/bin" -type f -name 'radio-*' -exec chmod 0755 {} +
